f4b0813a2ed930978189d86a8dd8c72f2aff13d5
Musbah Abu Shaban <musba7.98@gmail.com>
2019-01-24 00:50:09 +0200

    First commit

--- Calender/ViewController.swift
+++ Calender/ViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lbl: UILabel!
+    
+    @IBOutlet weak var gregorianTF: UITextField!
+    
+    private var datePicker: UIDatePicker?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        lbl.text! = getDateTime()
+        gregorianTF.text! = getDateTime()
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        datePicker?.addTarget(self, action: #selector(ViewController.selectDate), for: .valueChanged)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.viewTaped(gestureRecognizer:)))
+        view.addGestureRecognizer(tapGesture)
+        
+        gregorianTF.inputView = datePicker
+        
     }
 
+    @objc func viewTaped(gestureRecognizer:UITapGestureRecognizer)  {
+        view.endEditing(true)
+    }
+    func getDateTime() -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/YYYY"
+        return formatter.string(from: Date())
+    }
+    
+    @IBAction func ConvertTime(_ sender: Any) {
+        lbl.text! = toIslamic()
+        
+    }
+    func toIslamic() -> String {
+        if gregorianTF.text!.isEmpty {return "Enter valid date"}
+        let text = gregorianTF.text!
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let GregorianDate = dateFormatter.date(from: text)
+        let islamic = Calendar(identifier: Calendar.Identifier.islamicUmmAlQura)
+        let components = (islamic as NSCalendar?)?.components(NSCalendar.Unit(rawValue: UInt.max), from: GregorianDate!)
+        let fina_date = "\(components!.day!)/\(components!.month!)/\(components!.year!)"
+        print("final Islamic Date :: \(fina_date)") // 18/9/1412
+        return fina_date
+        
+    }
 
+    @objc func selectDate(datePicker: UIDatePicker)  {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/YYYY"
+        gregorianTF.text! = formatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
 }
 
